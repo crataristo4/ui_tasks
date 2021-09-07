9b8f1ee37b4554e1e302f1e4d28bda8cd9a5fa89
Aristocrat <crataristo4@gmail.com>
2021-09-07 19:01:07 +0000

    added

--- lib/ui/home/main_page/main_page.dart
+++ lib/ui/home/main_page/main_page.dart
@@ -14,8 +14,20 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Row(
-          children: [buildYourStory()],
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              buildYourStory(),
+              buildBuildDiscoverCreators(),
+              buildStories(Colors.lightBlue, 'jonasnaah'),
+              buildStories(Colors.deepPurpleAccent, 'tweetasm'),
+              buildStories(Colors.deepPurpleAccent, 'jonasnaah'),
+              buildStories(Colors.deepPurpleAccent, 'tweetasm'),
+              buildStories(Colors.deepPurpleAccent, 'jonasnaah'),
+              buildStories(Colors.deepPurpleAccent, 'tweetasm'),
+            ],
+          ),
         )
       ],
     );
@@ -28,7 +40,7 @@ class _MainPageState extends State<MainPage> {
           children: [
             Container(
               margin: EdgeInsets.only(
-                  top: sixteenDp, left: sixteenDp, right: sixteenDp),
+                  top: sixteenDp, left: sixteenDp, right: tenDp),
               height: hundredDp,
               width: hundredDp,
               decoration: BoxDecoration(
@@ -54,6 +66,72 @@ class _MainPageState extends State<MainPage> {
           ],
         ),
         Text(yourStory),
+      ],
+    );
+  }
+
+  Widget buildBuildDiscoverCreators() {
+    return Column(
+      children: [
+        Stack(
+          children: [
+            Container(
+              margin: EdgeInsets.only(
+                top: 30,
+                right: sixteenDp,
+                left: tenDp,
+              ),
+              height: eightyDp,
+              width: eightyDp,
+              decoration: BoxDecoration(
+                color: Colors.greenAccent,
+                borderRadius: BorderRadius.circular(thirtyDp),
+              ),
+            ),
+            Positioned(
+              top: twentyDp,
+              child: Container(
+                height: thirtyDp,
+                width: thirtyDp,
+                decoration: BoxDecoration(
+                    color: Colors.lightBlueAccent,
+                    borderRadius: BorderRadius.circular(twentyDp)),
+              ),
+            ),
+            Positioned(
+              top: eightyDp,
+              left: seventyDp,
+              child: Container(
+                height: thirtyDp,
+                width: thirtyDp,
+                decoration: BoxDecoration(
+                    color: Colors.deepPurpleAccent,
+                    borderRadius: BorderRadius.circular(twentyDp)),
+              ),
+            ),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 6),
+          child: Text(discoverCreators),
+        ),
+      ],
+    );
+  }
+
+  Widget buildStories(Color color, name) {
+    return Column(
+      children: [
+        Container(
+          margin: EdgeInsets.only(top: sixteenDp, right: tenDp),
+          height: hundredDp,
+          width: hundredDp,
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(thirtySixDp),
+          ),
+        ),
+        Text(name),
       ],
     );
   }
