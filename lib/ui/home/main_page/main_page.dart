f56f17137ea477c224458188e93c6f1af5938170
Aristocrat <crataristo4@gmail.com>
2021-09-07 17:16:44 +0000

    added build your story widget

---
+++ lib/ui/home/main_page/main_page.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:ui/constants/dimens.dart';
+import 'package:ui/constants/strings.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [buildYourStory()],
+        )
+      ],
+    );
+  }
+
+  Widget buildYourStory() {
+    return Column(
+      children: [
+        Stack(
+          children: [
+            Container(
+              margin: EdgeInsets.only(
+                  top: sixteenDp, left: sixteenDp, right: sixteenDp),
+              height: hundredDp,
+              width: hundredDp,
+              decoration: BoxDecoration(
+                color: Colors.brown,
+                borderRadius: BorderRadius.circular(thirtySixDp),
+              ),
+            ),
+            Positioned(
+              top: eightyDp,
+              left: ninetyDp,
+              child: Container(
+                child: Icon(
+                  Icons.add,
+                  color: Colors.white,
+                ),
+                height: thirtySixDp,
+                width: thirtySixDp,
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(twentyDp)),
+              ),
+            ),
+          ],
+        ),
+        Text(yourStory),
+      ],
+    );
+  }
+}
