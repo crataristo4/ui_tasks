b18d5734bf727fc71dcdab64a654fcc64e913597
Aristocrat <crataristo4@gmail.com>
2021-09-11 09:50:41 +0000

    modified

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -1,5 +1,6 @@
 const Main = 'Main';
 const trending = 'Trending';
+const feed = 'Feed';
 const explore = 'Explore';
 const yourStory = 'Your Story';
 const discoverCreators = 'Discover Creators';
@@ -7,4 +8,6 @@ const ownedBy = 'Owned by';
 const likedBy = 'Liked by';
 const others = 'Others';
 const dummy =
+    'Eelitias architecto voluptate aliquam sds sdfsd  Dsfdasf adfd daf  dsfa dssatretaed ';
+const dummyDes =
     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitiauas vel sint commodi repudiandae consequuntur voluptatum laborumnumquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentiumoptio, eaque rerum! Provident similique accusantium nemo autem. Veritatisobcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam';
---
+++ lib/constants/theme_color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ThemeColors {
+  static Color tealInactive = Color(0xFF0898A0).withOpacity(0.1);
+  static Color teal = Color(0xFF0898A0);
+  static Color black = Color(0xB6302626);
+}
